b100ce7d9c09480ee9fd4a9100e7ec251a9461db
fwbrasil <fwbrasil@gmail.com>
2018-04-09 22:03:45 -0700

    0.1.6

--- build/build.sh
+++ build/build.sh
@@ -2,7 +2,7 @@
 
 set -e # Any subsequent(*) commands which fail will cause the shell script to exit immediately
 
-SBT_CMD="sbt ++$TRAVIS_SCALA_VERSION +clean +coverage +test +coverageReport +coverageAggregate tut checkUnformattedFiles"   
+SBT_CMD="sbt ++$TRAVIS_SCALA_VERSION clean coverage test coverageReport coverageAggregate tut checkUnformattedFiles"   
 SBT_PUBLISH=" +coverageOff +publish"
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]
