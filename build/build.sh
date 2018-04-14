f5c3778a06592d58dd070c11a46b3d0e2a84fcb4
fwbrasil <fwbrasil@gmail.com>
2018-04-13 21:22:20 -0700

    fix cross publishing

--- build/build.sh
+++ build/build.sh
@@ -2,8 +2,8 @@
 
 set -e # Any subsequent(*) commands which fail will cause the shell script to exit immediately
 
-SBT_CMD="sbt ++$TRAVIS_SCALA_VERSION clean coverage test coverageReport coverageAggregate tut checkUnformattedFiles"   
-SBT_PUBLISH=" +coverageOff +publish"
+SBT_CMD="sbt clean coverage test coverageReport coverageAggregate tut checkUnformattedFiles"   
+SBT_PUBLISH=" coverageOff publish"
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]
 then
