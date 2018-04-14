3a97e03c48292655c77d61aa8c8ad3823635ef30
fwbrasil <fwbrasil@gmail.com>
2018-04-13 23:01:12 -0700

    testing local release

--- build/build.sh
+++ build/build.sh
@@ -15,24 +15,28 @@ then
 
     if [[ $TRAVIS_BRANCH == "master" && $(cat version.sbt) != *"SNAPSHOT"* ]]
     then
-        eval "$(ssh-agent -s)"
-        chmod 600 local.deploy_key.pem
-        ssh-add local.deploy_key.pem
-        git config --global user.name "Quill CI"
-        git config --global user.email "quillci@getquill.io"
-        git remote set-url origin git@github.com:traneio/arrows.git
-        git checkout master || git checkout -b master
-        git reset --hard origin/master
-        git push --delete origin website || true
+        # eval "$(ssh-agent -s)"
+        # chmod 600 local.deploy_key.pem
+        # ssh-add local.deploy_key.pem
+        # git config --global user.name "Quill CI"
+        # git config --global user.email "quillci@getquill.io"
+        # git remote set-url origin git@github.com:traneio/arrows.git
+        # git checkout master || git checkout -b master
+        # git reset --hard origin/master
+        # git push --delete origin website || true
 
+        echo 'Performing a release'
         sbt 'release cross with-defaults'
     elif [[ $TRAVIS_BRANCH == "master" ]]
     then
+        echo 'Master build'
         $SBT_CMD
     else
+        echo 'Branch build'
         echo "version in ThisBuild := \"$TRAVIS_BRANCH-SNAPSHOT\"" > version.sbt
         $SBT_CMD
     fi
 else
+    echo 'PR build'
     $SBT_CMD
 fi
