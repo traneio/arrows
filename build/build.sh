b814950200d66b7ae57f25736dcedebb5f9edd9c
fwbrasil <fwbrasil@gmail.com>
2018-04-13 22:22:43 -0700

    trying to fix cross release

--- build/build.sh
+++ build/build.sh
@@ -2,8 +2,8 @@
 
 set -e # Any subsequent(*) commands which fail will cause the shell script to exit immediately
 
-SBT_CMD="sbt ++$TRAVIS_SCALA_VERSION clean coverage test coverageReport coverageAggregate tut checkUnformattedFiles"   
-SBT_PUBLISH=" coverageOff publish"
+SBT_CMD="sbt +clean +coverage +test coverageReport coverageAggregate tut checkUnformattedFiles"   
+SBT_PUBLISH=" +coverageOff +publish"
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]
 then
@@ -25,7 +25,7 @@ then
         git reset --hard origin/master
         git push --delete origin website || true
 
-        sbt ++$TRAVIS_SCALA_VERSION 'release with-defaults skip-tests'
+        sbt 'release cross with-defaults'
     elif [[ $TRAVIS_BRANCH == "master" ]]
     then
         $SBT_CMD
